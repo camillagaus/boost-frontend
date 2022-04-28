9299ebb3bbd62704f1df12bf5680be0ec6817821
Camilla Gausland <camillagausland@MacBook-Pro-som-tillhor-Camilla.local>
2022-04-28 13:11:00 +0200

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
+    server: {
+        port: 8000 // default: 3000
+      }
 })
